3998645ade01db336202e2d7556f91d950442577
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-09-09 08:06:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |key2, value2|
+      value2.map do |string|
+        pigeon_list[string] = {}
+      end
+    end
+  end
+  return pigeon_list
 end
